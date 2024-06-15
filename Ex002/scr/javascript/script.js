ddc93a9b8954113d3b7ca46a6327d177de3ce4a4
Vanessa <vs6615302@gmail.com>
2024-06-14 23:03:13 -0300

    Alteração no ex002

---
+++ Ex002/scr/javascript/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#btn_mobile').on('click', function() {
+    $('#mobile_menu').toggleClass('active');
+    $('#btn_mobile').find('i').toggleClass('fa-x');
+  });
+
+
+});
