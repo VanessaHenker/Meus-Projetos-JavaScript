a58694e2f93e2b34347ae1ded461d9087f004c23
Vanessa <vs6615302@gmail.com>
2024-06-18 15:59:17 -0300

    Alteração no ex002

--- Ex002/scr-menu/script/script.js
+++ Ex002/scr-menu/script/script.js
@@ -1,6 +1,22 @@
+//buttons header
 $(document).ready(function() {
-  $('#btn_mobile').on('click', function() {
+  $('#btn_mobile')('#btn_mobile_pesq').on('click', function() {
+    $('#mobile_menu').toggleClass('active');
+    $('#btn_mobile').find('i').toggleClass('fa-x');
+
     $('#mobile_menu').toggleClass('active');
     $('#btn_mobile').find('i').toggleClass('fa-x');
   });
+});
+
+const searchIcon = document.querySelector('.search-icon');
+const searchBar = document.getElementById('search-bar');
+
+searchIcon.addEventListener('click', function() {
+  if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+    searchBar.style.display = 'block'; // Mostrar a barra de pesquisa
+    searchBar.focus(); // Colocar o foco na barra de pesquisa
+  } else {
+    searchBar.style.display = 'none'; // Ocultar a barra de pesquisa
+  }
 });
