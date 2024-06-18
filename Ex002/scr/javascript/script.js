c219ff76545a70e467b1a2f7c9544b7ff3aaede3
Vanessa <vs6615302@gmail.com>
2024-06-18 10:58:43 -0300

    Continuação do ex002

--- Ex002/scr/javascript/script.js
+++ Ex002/scr/javascript/script.js
@@ -3,4 +3,58 @@ $(document).ready(function() {
     $('#mobile_menu').toggleClass('active');
     $('#btn_mobile').find('i').toggleClass('fa-x');
   });
+
+  const sections = $('section');
+  const navItems = $('.nav-item');
+
+  $(window).on('scroll', function () {
+    const header = $('header');
+    const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+    let activeSectionIndex = 0;
+
+    if (scrollPosition <= 0) {
+      header.css('box-shadow', 'none');
+    } else {
+       header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
+    }
+
+    sections.each(function(i) {
+      const section = $(this);
+      const sectionTop = section.offset().top - 96;
+      const sectionBottom = sectionTop+ section.outerHeight();
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+      activeSectionIndex = i;
+        return false;
+        }
+      })
+
+      navItems.removeClass('active');
+      $(navItems[activeSectionIndex]).addClass('active');
+    });
+
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#cardapio', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#imagem-chefe', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    })
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
 });
