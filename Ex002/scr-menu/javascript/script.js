b3f633d8e46f4863b0667a33ecddd178180d4cf5
Vanessa <vs6615302@gmail.com>
2024-06-21 15:32:56 -0300

    Alteração no ex002

--- Ex002/scr-menu/javascript/script.js
+++ Ex002/scr-menu/javascript/script.js
@@ -7,9 +7,36 @@ $(document).ready(function () {
   });
 });
 
+  //button pesquisa
+  let boxBuscar = document.querySelector('.conteudo-button-pesq');
 
+  let lupa = document.querySelector('.btn_mobile_pesq');
 
- 
+  let btnFechar = document.querySelector('.button_fechar');
 
+  lupa.addEventListener('click', () => {
+    boxBuscar.classList.add('ativar');
+  });
+
+  btnFechar.addEventListener('click', () => {
+    boxBuscar.classList.remove('ativar');
+  });
+   
+  //button pesquisa opções
+  const searchBar = document.getElementById('barra-pesquisa');
+  const options = document.getElementById('opcoes');
+  
+  searchBar.addEventListener('click', () => {
+    options.classList.remove('ativar-barra');
+  });
+  
+   // Adiciona comportamento de clique nas opções
+  const optionItems = document.querySelectorAll('.opcoes-barra');
+  optionItems.forEach(option => {
+    option.addEventListener('click', () => {
+      searchBar.value = option.textContent;
+      options.classList.add('ativar-barra');           
+    })
+  });
 
 
