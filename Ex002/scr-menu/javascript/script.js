6c2022e30cf2351700828fe985442f38ec6299a7
Vanessa <vs6615302@gmail.com>
2024-06-21 16:32:34 -0300

    Alteração no ex002

--- Ex002/scr-menu/javascript/script.js
+++ Ex002/scr-menu/javascript/script.js
@@ -1,11 +1,11 @@
-// header
-//buttom menu
-$(document).ready(function () {
-  $('#btn_mobile').on('click', function () {
-    $('#mobile_menu').toggleClass('active');
-    $('#btn_mobile').find('i').toggleClass('fa-x');
+  // header 
+  //buttom menu
+  $(document).ready(function () {
+    $('#btn_mobile').on('click', function () {
+      $('#mobile_menu').toggleClass('active');
+      $('#btn_mobile').find('i').toggleClass('fa-x');
+    });
   });
-});
 
   //button pesquisa
   let boxBuscar = document.querySelector('.conteudo-button-pesq');
@@ -14,28 +14,31 @@ $(document).ready(function () {
 
   let btnFechar = document.querySelector('.button_fechar');
 
+  //Abre a barra de pesquisa
   lupa.addEventListener('click', () => {
     boxBuscar.classList.add('ativar');
   
   });
 
+  //Remove a barra de pesquisa
   btnFechar.addEventListener('click', () => {
     boxBuscar.classList.remove('ativar');
   })
 
-   
   //button pesquisa opções
   const searchBar = document.getElementById('barra-pesquisa');
   const options = document.getElementById('opcoes');
   const button_close = document.getElementById('button_fechar');
 
+  //Abre as opções
   searchBar.addEventListener('click', () => {
     options.classList.remove('ativar-barra');
   });
   
+  //Remove as opções clicando no X
   button_close.addEventListener('click', () => {
     options.classList.add('ativar-barra');
-  });
+  }); 
   
   // Adiciona comportamento de clique nas opções
   const optionItems = document.querySelectorAll('.opcoes-barra');
@@ -44,4 +47,14 @@ $(document).ready(function () {
       searchBar.value = option.textContent;
       options.classList.add('ativar-barra');           
     })
+  });
+
+  //verificar se opção de busca está aberta
+
+  document.addEventListener('click', function(event) {
+    var isClickInside = content.contains(event.target);
+    
+    if (!isClickInside) {
+      boxBuscar.classList.remove('ativar');
+    } 
   });
