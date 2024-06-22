a439f41a4202ec50dd502a8f2cf131013414c566
Vanessa <vs6615302@gmail.com>
2024-06-22 18:58:04 -0300

    Alteração no ex002

--- Ex002/scr-menu/javascript/script.js
+++ Ex002/scr-menu/javascript/script.js
@@ -5,6 +5,17 @@
       $('#mobile_menu').toggleClass('active');
       $('#btn_mobile').find('i').toggleClass('fa-x');
     });
+
+    // Manipulador de eventos para o clique no documento
+    $(document).on('click', function (event) {
+      var $target = $(event.target);
+    
+    // Verifica se o clique foi fora do menu e do botão
+      if (!$target.closest('#mobile_menu').length && !$target.closest('#btn_mobile').length) {
+        $('#mobile_menu').removeClass('active');
+        $('#btn_mobile').find('i').removeClass('fa-x');
+      }
+    });
   });  
 
   //button pesquisa
