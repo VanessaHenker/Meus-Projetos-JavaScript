5c19cceee357385d3de3a7614ec031763a68ba00
Vanessa <vs6615302@gmail.com>
2024-07-09 16:51:26 -0300

    Alteração no ex002

--- Ex002/scr-menu/javascript/script.js
+++ Ex002/scr-menu/javascript/script.js
@@ -87,8 +87,9 @@
     }
   }
 
-  //Conteudo informação
-  
- 
+//Conteudo informação
+let now = new Date();
 
- 
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getSeconds();
