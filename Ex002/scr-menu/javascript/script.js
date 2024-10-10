08776b15e1ac8e3ee688f9640650a4ebe1699398
Vanessa <vs6615302@gmail.com>
2024-10-09 21:17:02 -0300

    Ultima alteração

--- Ex002/scr-menu/javascript/script.js
+++ Ex002/scr-menu/javascript/script.js
@@ -574,19 +574,3 @@ function slideCarousel(categoriaIndex, direction) {
   carousel.style.transform = `translateX(${currentOffset}px)`;
   carousel.setAttribute('data-offset', currentOffset.toString());
 }
-
-// Chamar a função para carregar o cardápio
-carregarCardapio('caminho/para/seu/json/cardapio.json');
-// Comportamento de clique para cada categoria
-categoriaElemento.addEventListener('click', () => {
-  searchBar.value = categoria.nome; // Define o valor do input como o nome da categoria
-  options.classList.add('ativar-barra'); // Fecha as opções após a seleção
-
-  // Encontra o título correspondente à categoria no cardápio
-  const tituloCategoria = document.querySelector(`h3.section-subtitulo:contains('${categoria.nome}')`);
-  
-  if (tituloCategoria) {
-    // Faz o scroll até o título da categoria
-    tituloCategoria.scrollIntoView({ behavior: 'smooth' });
-  }
-});
